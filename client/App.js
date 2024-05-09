d9459d881cc0fbcdab7dba7f1093f337c144adf7
salvarsa <salvarsa.dev13@gmail.com>
2024-05-09 13:33:16 -0500

    feat: se conecta el backend

--- client/App.js
+++ client/App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
 
 export default function App() {
+
+  const [task, setTask] = useState()
+
+  async function fetchData(){
+    const response = await fetch('http://localhost:2402/api/v1/tasks')
+    const data = await response.json()
+    setTask(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{JSON.stringify(task)}</Text>
       <StatusBar style="auto" />
     </View>
   );
